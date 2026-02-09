d173a49a9e8b418efaeeb01a65e5d9866f6e1818
Kyle Brewer <kyle.brewer23@my.northampton.ac.uk>
2026-02-09 16:49:19 +0000

    New terrains and sniper scope and sniper anims

--- Sniper Sim Project/Assets/Scripts/Bullet_Controller.cs
+++ Sniper Sim Project/Assets/Scripts/Bullet_Controller.cs
@@ -1,4 +1,3 @@
-using UnityEditor.Rendering.Analytics;
 using UnityEngine;
 
 public class Bullet_Controller : MonoBehaviour
@@ -6,47 +5,36 @@ public class Bullet_Controller : MonoBehaviour
     [SerializeField] private float Speed;
     [SerializeField] private float Weight;
     [SerializeField] private float Life_Time;
-    [SerializeField] private Vector3 Start_Pos;
-    [SerializeField] private Vector3 Forward;
+    [SerializeField] private Vector3 OldPos;
+    [SerializeField] private Vector3 New;
     private RaycastHit hit;
+    private Rigidbody rb;
+    [SerializeField] private Transform ForcePosition;
+
+    private void Start()
+    {
+
+    }
+
     public void Initialize(Transform StartPos)
     {
-        Start_Pos = StartPos.position;
-        Forward = StartPos.forward;
+        New  = transform.position;
+        rb = GetComponent<Rigidbody>();
+        rb.AddExplosionForce(3000, ForcePosition.position, 1);
     }
 
-    private Vector3 GetPositionOnArc(float Time)
+    private void RayCheck()
     {
-        Vector3 point = Start_Pos + (Forward *Speed * Life_Time);
-        Vector3 GravityVector = Vector3.down * Weight * Time * Time;
-        return point + GravityVector;
-    }
-
-    private bool CastRayBetweenPoints(Vector3 start, Vector3 End, out RaycastHit hit)
-    {
-        return Physics.Raycast(start, End - start, out hit, (End-start).magnitude);
-    }
-
-    private void FixedUpdate()
-    {
-        if(Life_Time > 0) {Life_Time -= Time.deltaTime; }
-
-        float currentTime = Time.time - Life_Time;
-        float nextTime = currentTime + Time.fixedDeltaTime;
-
-        Vector3 currentPoint = GetPositionOnArc(currentTime);
-        Vector3 nextPoint = GetPositionOnArc(nextTime);
-
-        if(CastRayBetweenPoints(currentPoint, nextPoint, out hit))
+        OldPos = New;
+        New = transform.position;
+        if(Physics.Raycast(OldPos, New, (OldPos - New).magnitude))
         {
-            print("hit");
+
         }
     }
 
-    private void Update()
-    {
-        float currentTime = Time.time - Life_Time;
-        Vector3 currentPoint = GetPositionOnArc(currentTime);
-        transform.position = currentPoint;
+    private void FixedUpdate()
+    { 
+        RayCheck();
     }
 }
