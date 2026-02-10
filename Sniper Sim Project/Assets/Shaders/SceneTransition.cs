5947c6accbc13a4b604ef671e49a5e4b502e3a45
Kyle Brewer <kyle.brewer23@my.northampton.ac.uk>
2026-02-10 22:07:06 +0000

    Transition out image

--- Sniper Sim Project/Assets/Shaders/SceneTransition.cs
+++ Sniper Sim Project/Assets/Shaders/SceneTransition.cs
@@ -5,25 +5,46 @@ using UnityEngine.Events;
 public class SceneTransition : MonoBehaviour
 {
     [SerializeField] private Material TransMat;
-    [SerializeField] private float TransTime = 1f;
+    [SerializeField] private float TransTimeIn = 1f;
     private string PropertyName = "_Progress";
     private bool Transitioning;
 
     public UnityEvent OnTransitionComplete;
 
-    public void StartTransition()
+    public void StartTransitionIn()
     {
         if(Transitioning) { return; }
         StartCoroutine(TransitionIn());
     }
+
+    public void StartTransitionOut()
+    {
+        if (Transitioning) { return; }
+        StartCoroutine(TransitionOut());
+    }
+
     public IEnumerator TransitionIn()
     {
         Transitioning = true;
         float curTime = 0;
-        while (curTime < TransTime)
+        while (curTime < TransTimeIn)
         {
             curTime += Time.deltaTime;
-            TransMat.SetFloat(PropertyName, Mathf.Clamp01(curTime / TransTime));
+            TransMat.SetFloat(PropertyName, Mathf.Clamp01(curTime / TransTimeIn));
+            yield return null;
+        }
+        Transitioning = false;
+        OnTransitionComplete?.Invoke();
+    }
+
+    public IEnumerator TransitionOut()
+    {
+        Transitioning = true;
+        float curTime = 1f;
+        while (curTime > 0)
+        {
+            curTime -= Time.deltaTime;
+            TransMat.SetFloat(PropertyName, curTime);
             yield return null;
         }
         Transitioning = false;
