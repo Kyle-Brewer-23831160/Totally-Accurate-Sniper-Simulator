4265c2f9839414b7f02cef79aed0273e588ba2d1
Kyle Brewer <149398386+Kyle-Brewer-23831160@users.noreply.github.com>
2026-02-10 14:22:43 +0000

    Sniper scope in overlay logic and camera FOV logic added

--- Sniper Sim Project/Assets/Scripts/Sniper_Controller.cs
+++ Sniper Sim Project/Assets/Scripts/Sniper_Controller.cs
@@ -6,8 +6,11 @@ public class Sniper_Controller : MonoBehaviour
     [Header("Sniper Variables")]
     [SerializeField] private int Mag_Ammo;
     [SerializeField] private GameObject ShotPoint;
+    [SerializeField] private GameObject SniperBody;
     [SerializeField] private float Speed;
+    [SerializeField] private GameObject ScopeOverlay;
     private bool Shell_In_Chamber = true;
+    private Animator _Animator;
 
     [Header("Bullet Variables")]
     [SerializeField] private GameObject Bullet;
@@ -15,7 +18,7 @@ public class Sniper_Controller : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -56,15 +59,40 @@ public class Sniper_Controller : MonoBehaviour
 
     private void ScopeAnimController()
     {
-        if (Input.GetMouseButton(1))
+        if (_Animator.GetFloat("Blend") == 1)
         {
-            if (GetComponent<Animator>().GetFloat("Blend") >= 1) return;
-            GetComponent<Animator>().SetFloat("Blend", GetComponent<Animator>().GetFloat("Blend") + Speed);
+            if (SniperBody.transform.GetChild(SniperBody.transform.childCount - 1).gameObject.activeInHierarchy)
+            {
+                for (int i = 0; i < SniperBody.transform.childCount; i++)
+                {
+                    SniperBody.transform.GetChild(i).gameObject.SetActive(false);
+                }
+                ScopeOverlay.SetActive(true);
+                Camera.main.fieldOfView = 0.5f;
+            }
         }
         else
         {
-            if (GetComponent<Animator>().GetFloat("Blend") <= 0) return;
-            GetComponent<Animator>().SetFloat("Blend", GetComponent<Animator>().GetFloat("Blend") - Speed);
+            if (!SniperBody.transform.GetChild(SniperBody.transform.childCount - 1).gameObject.activeInHierarchy)
+            {
+                for (int i = 0; i < SniperBody.transform.childCount; i++)
+                {
+                    SniperBody.transform.GetChild(i).gameObject.SetActive(true);
+                }
+                ScopeOverlay.SetActive(false);
+                Camera.main.fieldOfView = 35;
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            if (_Animator.GetFloat("Blend") >= 1) { _Animator.SetFloat("Blend", 1); return; }
+            _Animator.SetFloat("Blend", _Animator.GetFloat("Blend") + Speed);
+        }
+        else
+        {
+            if (_Animator.GetFloat("Blend") <= 0) { _Animator.SetFloat("Blend", 0); return; }
+            _Animator.SetFloat("Blend", _Animator.GetFloat("Blend") - Speed);
         }
     }
 }
